abed0808510746351aebdde8a6543dd6f4cda261
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2019-03-26 23:53:23 -0700

    error for inputRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import Input from '@material-ui/core/Input'
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const App = () => {
+  const myRef = React.useRef()
+
+  return (
+    <div className="App">
+      <input name="foo" ref={myRef}/>
+      <Input inputRef={myRef}></Input>
+    </div>
+  );
 }
 
 export default App;
